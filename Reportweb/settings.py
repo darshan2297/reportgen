84a581975782d0a741d0cea8c0233de142c169e2
darshan2297 <darshanvankawala2297@gmail.com>
2020-02-12 19:52:47 +0530

    first commit

--- Reportweb/settings.py
+++ Reportweb/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
